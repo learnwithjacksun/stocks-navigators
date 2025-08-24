679237fd843529ec1e0e5c464e610857f0036fa9
learnwithjacksun <codewithjacksun@gmail.com>
2025-08-24 14:15:37 +0100

    Refactor Home component to simplify layout and enhance user experience. Removed unnecessary elements and added logo, welcome message, and navigation links for login and registration.

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,61 +1,17 @@
-import ModeToggle from "@/components/ui/mode-toggle";
-import { libraries } from "@/constants/data";
-import { Github, ArrowRight } from "lucide-react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <>
-      <div className="min-h-screen w-full bg-background relative">
-        <div
-          className="absolute inset-0 z-0"
-          style={{
-            backgroundImage: `
-        linear-gradient(to right, var(--foreground) 1px, transparent 1px),
-        linear-gradient(to bottom, var(--foreground) 1px, transparent 1px)
-      `,
-            backgroundSize: "20px 30px",
-            WebkitMaskImage:
-              "radial-gradient(ellipse 70% 60% at 50% 0%, #000 60%, transparent 100%)",
-            maskImage:
-              "radial-gradient(ellipse 70% 60% at 50% 0%, #000 60%, transparent 100%)",
-          }}
-        />
-
-        <div className="h-[100dvh] relative z-10 center flex-col gap-10 text-center layout">
-          <div className="center gap-2">
-            <a
-              href="https://github.com/learnwithjacksun/my-react-template"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="border border-line rounded-full bg-secondary/70 px-4 py-2 center gap-20 inline-flex "
-            >
-              <div className="center gap-2">
-                <Github size={18} className="text-main/70" />
-                <p className="text-sm">Use Template</p>
-              </div>
-              <ArrowRight size={20} className="text-main/70" />
-            </a>
-          </div>
-          <div className="space-y-2">
-            <h1 className="text-6xl md:leading-[80px] leading-[60px] font-bold text-transparent bg-clip-text bg-gradient-to-r from-main to-main/70 dark:to-main/50">
-              My Reactjs Template
-            </h1>
-            <p className="text-muted text-sm">With typescript.</p>
-          </div>
-
-          <ul className="center flex-wrap gap-2">
-            {libraries.map((library) => (
-              <li
-                key={library}
-                className="text-sm text-muted dark:bg-secondary border border-line rounded-full px-4 py-2"
-              >
-                {library}
-              </li>
-            ))}
-          </ul>
-          <ModeToggle />
-        </div>
+    <div className="bg-foreground min-h-[100dvh] center flex-col gap-4 main">
+      <img src="/logo.webp" alt="logo" width={200} />
+      <h1 className="text-2xl font-bold">Home Page</h1>
+      <p className="text-sm text-muted">
+        Welcome to the home page of the Stocks Navigators.
+      </p>
+      <div className="flex gap-4">
+        <Link to="/login" className="btn">Login</Link>
+        <Link to="/register" className="btn">Register</Link>
       </div>
-    </>
-  );
+    </div>
+  )
 }
